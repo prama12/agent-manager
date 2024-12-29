e448b8ce54567490e6f4203bb10c6b543c7537b3
prama12 <panditprama@gmail.com>
2024-12-29 11:16:21 +0545

    project setup

---
+++ src/components/layout/MainLayout.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./Navbar";
+
+const MainLayout = () => {
+  return (
+    <>
+      <div>
+        <Navbar />
+      </div>
+    </>
+  );
+};
+
+export default MainLayout;
---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import Navbar from "@/components/layout/Navbar";
+import React from "react";
+
+const index = () => {
+  return (
+    <>
+      <div>
+        <Navbar />
+      </div>
+    </>
+  );
+};
+
+export default index;
